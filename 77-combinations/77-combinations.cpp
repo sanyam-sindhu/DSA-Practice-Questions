c0d648dcb6179cc9805fa09e02c371f2e742b332
Sanyam Sindhu <89131737+sanyam-sindhu@users.noreply.github.com>
2022-05-31 23:35:35 +0530

    Time: 23 ms (87.01%), Space: 9.1 MB (66.61%) - LeetHub

---
+++ 77-combinations/77-combinations.cpp
@@ -0,0 +1,22 @@
+class Solution {
+private:
+    void backtrack(vector<vector<int>>&ans,vector<int>&curr,int n,int k,int index){
+        if(curr.size()==k){
+            ans.push_back(curr);
+            return;
+        } else {
+            for(int i=index;i<=n;i++){
+                curr.push_back(i);
+                backtrack(ans,curr,n,k,i+1);
+                curr.pop_back();
+            }
+        }
+    }
+public:
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>>ans;
+        vector<int>curr;
+        backtrack(ans,curr,n,k,1);
+        return ans;
+    }
+};
