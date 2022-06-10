e8d40258aedad567b52c83bf3dc854dbb8f6edfd
Sanyam Sindhu <89131737+sanyam-sindhu@users.noreply.github.com>
2022-06-10 17:53:53 +0530

    Time: 342 ms (28.28%), Space: 65 MB (59.00%) - LeetHub

--- 886-possible-bipartition/886-possible-bipartition.cpp
+++ 886-possible-bipartition/886-possible-bipartition.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     
-    bool isBipartite(vector<vector<int>>&adj, vector<int>&color, int n, int x){
+    /*bool isBipartite(vector<vector<int>>&adj, vector<int>&color, int n, int x){
         queue<int>q;
         q.push(x);
         color[x] = 1;
@@ -18,10 +18,28 @@ public:
             }
         }
         return true;
-    }
+    }*/
+    // dfs
+    bool bipartiteDfs(int node, vector<vector < int>> &adj, vector<int> &color)
+        {
+            for (auto it: adj[node])
+            {
+                if (color[it] == -1)
+                {
+                    color[it] = 1 - color[node];
+                    if (!bipartiteDfs(it, adj, color))
+                    {
+                        return false;
+                    }
+                }
+                else if (color[it] == color[node]) return false;
+            }
+            return true;
+        }
+        
     
     bool possibleBipartition(int n, vector<vector<int>>& dislikes) {
-        vector<int>color(n+1, 0);
+        vector<int>color(n+1, -1);
         
         vector<vector<int>>adj(n+1);
         for(int i=0;i<dislikes.size();i++){
@@ -30,8 +48,10 @@ public:
         }
         
         for(int i=1;i<=n;i++){
-            if(color[i]==0){
-                if(!isBipartite(adj, color, n, i)){
+            if(color[i]==-1){
+                color[i]=1;
+                
+                if(!bipartiteDfs(i,adj,color)){
                     return false;
                 }
             }
