7dba95b5b1ea6e310c0a0b15af1ae3575f62b350
Sanyam Sindhu <89131737+sanyam-sindhu@users.noreply.github.com>
2022-06-20 03:05:51 +0530

    Time: 352 ms (5.04%), Space: 14.5 MB (20.75%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -18,18 +18,18 @@ public:
                 }
             }
         }
-         for (int i = n - 2; i >= 0; i--) {
-    for (int j1 = 0; j1<m; j1++) {
-      for (int j2 = 0;j2<m; j2++) {
+         for (int i=n-2;i>= 0;i--) {
+            for (int j1 =0;j1<m; j1++) {
+                for (int j2=0;j2<m; j2++) {
 
         int mx = INT_MIN;
         for (int di=-1;di<=1;di++) {
           for (int dj=-1;dj<=1;dj++) {
             int sum;
-            if (j1 == j2)
+            if (j1==j2)
               sum=grid[i][j1];
             else
-              sum= grid[i][j1] + grid[i][j2];
+              sum= grid[i][j1]+grid[i][j2];
 
             if ((j1+di<0 ||j1+di>m-1) ||(j2+dj<0||j2+dj>m-1))
                 sum+=-1e9;
