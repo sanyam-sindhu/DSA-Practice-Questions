883fd197802cf157e70e35593b829e0d5b6bb44b
Sanyam Sindhu <89131737+sanyam-sindhu@users.noreply.github.com>
2022-06-20 23:02:56 +0530

    Time: 263 ms (40.28%), Space: 82.9 MB (77.47%) - LeetHub

---
+++ 41-first-missing-positive/41-first-missing-positive.cpp
@@ -0,0 +1,15 @@
+class Solution
+{
+    public:
+    int firstMissingPositive(vector<int> &nums)
+    {
+        sort(nums.begin(), nums.end());
+        int start=1;
+        for (int i=0; i<nums.size();i++)
+        {
+            if (nums[i]==start)
+                start++;
+        }
+        return start;
+    }
+};
