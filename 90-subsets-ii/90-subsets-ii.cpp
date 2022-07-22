e63b2c3a8fda9917c6f69da556d874fd525431db
Sanyam Sindhu <89131737+sanyam-sindhu@users.noreply.github.com>
2022-07-22 12:38:02 +0530

    Time: 3 ms (82.53%), Space: 7.9 MB (49.41%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
-    set<vector<int> >v1;
-    void subseq(int ind,vector<vector<int>> &finalv,vector<int> &v,vector<int> &nums)
+    void solve(int ind,vector<int> &nums,vector<int> &v1,set<vector<int>> &v,int n)
     {
-        int n=nums.size();
         if(ind>=n)
         {
+            v.insert(v1);
             return ;
+            
         }
-        v.push_back(nums[ind]);
-        v1.insert(v);
-        subseq(ind+1,finalv,v,nums);
-        v.pop_back();
-        subseq(ind+1,finalv,v,nums);
+        v1.push_back(nums[ind]);
+        solve(ind+1,nums,v1,v,n);
+        v1.pop_back();
+        solve(ind+1,nums,v1,v,n);
+        
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         sort(nums.begin(),nums.end());
-        vector<int> v;
-        vector<vector<int>> ans;
-        subseq(0,ans,v,nums);
-        ans.push_back({});
-        for(auto i:v1)
-        {
-            ans.push_back(i);
-        }
-        return ans;
+        int n=nums.size();
+        set<vector<int>> v;
+        vector<int> v1;
+        solve(0,nums,v1,v,n);
+        vector<vector<int>> finalv(v.begin(),v.end());
+        return finalv;
         
         
     }
